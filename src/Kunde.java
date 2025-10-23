e137290f01ba74f02b083687ed7ba0dcb3d68585
LatinaEnjoyer <nioe1000@stud.ek.dk>
2025-10-23 14:21:02 +0200

    Kalender/Booking/Kunde klasser

---
+++ src/Kunde.java
@@ -0,0 +1,21 @@
+public class Kunde {
+    private String navn;
+    private String telefon;
+
+    public Kunde(String navn, String telefon){
+        this.navn = navn;
+        this.telefon = telefon;
+    }
+
+    public String toString(){
+        return navn + " ("+telefon+")";
+    }
+}
+
+class testing0{
+    public static void main(String[] args) {
+        String tlf = "20130021";
+        Kunde a = new Kunde("Nico", tlf);
+        System.out.println(a);
+    }
+}
